31fa89717f748231a8286421f940abe042b87688
Kaleb <kaleb792762@gmail.com>
2023-08-31 15:43:23 +0530

    Polymorphism Added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,22 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+        public int product(int x, int y) {
+            return 1;
+        }
+        public int product(int x, int y, int z) {
+            return 1;
+        }
+        public double product(double x, double y) {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(20,30);
+        p.product(20,30,40);
+        p.product(20.0,30.0);
+    }
 }
